2ebf3df220a7fcd27cc5b092ee2c7624ff8c0274
MackRoe <elaine.music@students.makeschool.com>
2019-10-28 16:25:23 -0700

    now prints backwards poem

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-poem = "If you are a dreamer, come in,
+poem = """If you are a dreamer, come in,
 If you are a dreamer, a wisher, a liar,
 A hope-er, a pray-er, a magic bean buyer…
 If you’re a pretender, come sit by my fire
 For we have some flax-golden tales to spin.
 Come in!
-Come in!"
+Come in!"""
 
 
-def lines_printed_backwards():
+def lines_printed_backwards(lines):
     '''This function takes in a list of strings containing the lines of your
     poem as arguments and will print the poem lines out in reverse with the
     line numbers reversed. '''
-    pass
+    lines.reverse()
+    for line in lines:
+        print(line)
 
 
 def lines_printed_random(lines):
@@ -19,7 +21,15 @@ def lines_printed_random(lines):
     random order. Repeats are okay and the number of lines printed should be
     equal to the original number of lines in the poem (line numbers don't need
     to be printed). Hint: try using a loop and randint()'''
-    pass
+
+    random_lines = []
+    while len(lines) < 0:
+        line = random.choice(lines)
+        lines.pop(line)
+        random_lines.append(line)
+        lines = random_lines
+    for line in lines:
+        print(line)
 
 
 def my_own_rearrange():
@@ -29,7 +39,14 @@ def my_own_rearrange():
 
 
 lines = poem.split("\n")
-print(lines)
+
+# make prettier output by seperating lines
+for line in lines:
+    print(line)
 
 # testing Code
-# lines_printed_backwards(lines)
+print("Backwards Poem:")
+lines_printed_backwards(lines)
+
+# print("Randomized Poem:")
+# lines_printed_random(lines)
